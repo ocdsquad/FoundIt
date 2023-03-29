00ea2132c0cbcd40079da4cdd5aea16e67f27b43
ahmad alfarel <84859336+ocdsquad@users.noreply.github.com>
2023-03-30 00:52:44 +0700

    model,migrate,controller

---
+++ app/Http/Controllers/BarangHilangController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BarangHilang;
+use App\Http\Requests\StoreBarangHilangRequest;
+use App\Http\Requests\UpdateBarangHilangRequest;
+
+class BarangHilangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBarangHilangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BarangHilang $barangHilang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BarangHilang $barangHilang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBarangHilangRequest $request, BarangHilang $barangHilang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BarangHilang $barangHilang)
+    {
+        //
+    }
+}
---
+++ database/seeders/BarangHilangSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BarangHilangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}

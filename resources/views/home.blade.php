15b7a8b4991b21ffccbe3295f87a36f8d5768ce7
ahmad alfarel <84859336+ocdsquad@users.noreply.github.com>
2023-04-02 14:18:42 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,9 +32,9 @@
 
                     <a class="text-sm font-montserrat font-semibold xl:text-base" href="#"> Laporan </a>
 
-                    <a class="text-sm font-montserrat font-semibold xl:text-base" href="#"> Chatting </a>
+                    <a class="text-sm font-montserrat font-semibold xl:text-base" href="/baranghilang"> Barang Hilang </a>
 
-                    <a class="text-sm font-montserrat font-semibold xl:text-base" href="#"> Settings </a>
+                    <a class="text-sm font-montserrat font-semibold xl:text-base" href="/barangtemu"> Barang Temuan </a>
 
                     <form action="/baranghilang" class="w-[35%] relative xl:w-[28%]">
                         <input id="search" name="search" class="w-full pl-12 py-2 text-xs font-poppins font-medium placeholder-[#244CA5] bg-white border border-[#244CA5] rounded-lg xl:text-sm" type="text" placeholder="Cari Barang!">

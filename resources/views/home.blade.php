d73cf8f8ee34c8d12dae22e143c9c504ee8a1692
ahmad alfarel <84859336+ocdsquad@users.noreply.github.com>
2023-04-11 17:25:47 +0700

    kontol

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -571,6 +571,18 @@
                                 <!-- Nama Barang -->
                                 <h1 class="mb-2 text-base font-poppins font-semibold lg:text-lg"> {{ $barang->nama }}</h1>
 
+                                <!-- Location Barang -->
+                                <div class="flex items-center w-full mb-[5px]">
+                                    <span class="mr-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[15px] h-[15px]">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                        </svg>
+                                    </span> 
+
+                                    <p class="text-xs font-poppins font-normal lg:text-sm"> {{ $barang->lokasi }} </p>
+                                </div>
+
                                 <!-- Tanggal Barang -->
                                 <div class="flex items-center w-full mb-3">
                                     <span class="mr-[5px]">
